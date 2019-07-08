779f47fefacab78ef4d6f9e93593881fb9a430b3
Ken Stowell <ken@liveaxle.com>
2019-07-08 13:59:28 -0700

    Adding testing infrastructure with a few specs.

---
+++ spec/index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+/***********************************************************************************************************************************************
+ * @LIVEAXLE/NODE-PG-MIGRATE - SPEC
+ ***********************************************************************************************************************************************
+ * @description
+ */
+const path = require('path');
+const Mocha = require('mocha');
+const fs = require('fs');
+const mocha = new Mocha({reporter: 'nyan', fullTrace: false});
+const args = require('minimist')(process.argv.slice(2));
+const {exec} = require('child_process');
+
+/**
+ * Global helper api.
+ */
+global.runner = {
+  stub: notYetImplementedStub,
+  reset: (cb) => {
+    exec('rm -rf ./migrations', () => {
+      setTimeout(cb, 1000);
+    });
+  }
+};
+
+//
+// SPECS
+//------------------------------------------------------------------------------------------//
+// @description
+//
+const specs = {
+  migrate: path.join(__dirname, 'migrate.js')
+};
+
+//
+// RUNNER
+//------------------------------------------------------------------------------------------//
+// @description
+Object.keys(specs).forEach(spec => mocha.addFile(specs[spec]));
+
+// Run specs
+mocha.run((failures=0) => {
+  // essentially, for CI we can opt in to break the process on test fails,
+  //but for dev it's a little tedious
+  if(!args['brk-failures']) return;
+
+  process.on('exit', function () {
+    process.exit(failures);  // exit with non-zero status if there were failures - mainly for CI.
+  });
+});
+
+
+//
+// RUNNER HELPERS
+//------------------------------------------------------------------------------------------//
+// @description
+//
+function notYetImplementedStub(done) {
+  throw new Error('Test not yet implemented');
+  // done();
+}
---
+++ spec/migrate.js
@@ -0,0 +1,60 @@
+'use	strict';
+
+/***********************************************************************************************************************************************
+  MIGRATE SPEC
+  ***********************************************************************************************************************************************
+  *	@description
+  */
+const chai = require('chai');
+const path = require('path');
+const fs = require('fs');
+const {exec} = require('child_process');
+
+//
+// Chai constants
+//------------------------------------------------------------------------------------------//
+// @description
+//
+const expect = chai.expect;
+const assert = chai.assert;
+
+/**
+ * 
+ */
+describe('Migrate', () => {
+
+  before((done) => {
+    global.runner.reset(done);
+  });
+
+  describe('Create',  () => {
+    it('Should create a migration with a timestamp.',  (done) => {
+      exec('node ./ create timestamp', (err, stdout, stderr) => {
+        let file =  stdout.match(/[0-9].*.timestamp.migration.js/gi);
+
+        assert.lengthOf(file, 1, 'File was not created.');
+        assert.isNumber(parseInt(file[0].split('.')[0]));
+
+        global.runner.reset(done);
+      });
+    });
+
+    it('Should create an ordered migration.',  (done) => {
+      exec('node ./ create ordered --ordering sequential', (err, stdout, stderr) => {
+        let matched = stdout.match(/01.ordered.migration.js/gi);
+
+        assert.lengthOf(matched, 1, 'File was not created.');
+        done();
+      });
+    });
+
+    it('Should create an ordered migration and it should be numbered after the previous one.',  (done) => {
+      exec('node ./ create ordered --ordering=sequential', (err, stdout, stderr) => {
+        let matched = stdout.match(/02.ordered.migration.js/gi);
+
+        assert.lengthOf(matched, 1, 'File was not created.');
+        done();
+      });
+    });
+  });
+});
